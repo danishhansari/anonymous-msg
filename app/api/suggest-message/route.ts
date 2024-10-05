58665e6243a37b88c1fc38a016f90d846228d9cd
Danish <dan71ish@gmail.com>
2024-10-05 14:43:36 +0530

    tried to implement open ai

---
+++ app/api/suggest-message/route.ts
@@ -0,0 +1,39 @@
+import OpenAI from "openai";
+import { OpenAIStream, StreamingTextResponse } from "ai";
+import { NextResponse } from "next/server";
+
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+export const runtime = "edge";
+
+export async function POST(request: Request) {
+  try {
+    const { messages } = await request.json();
+
+    const response = await openai.chat.completions.create({
+      model: "gpt-3.5-turbo",
+      stream: true,
+      messages,
+    });
+
+    const stream = OpenAIStream(response);
+
+    return new StreamingTextResponse(stream);
+  } catch (error) {
+    if (error instanceof OpenAI.APIError) {
+      const { name, status, headers, message } = error;
+      return NextResponse.json(
+        {
+          name,
+          status,
+          headers,
+          message,
+        },
+        { status }
+      );
+    }
+    throw new Error("Error while finding");
+  }
+}
